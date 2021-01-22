57f9a008946a2b92bedb38ef47ab556a1a395fd3
Ewann Pellé <ewskr0@gmail.com>
2021-01-22 09:52:42 +0100

    Adds init description

--- lib/main.cpp
+++ lib/main.cpp
@@ -5,13 +5,25 @@
 #include "player.cpp"
 #include "utils.cpp"
 
-int main(int argc, char const *argv[]) {
-  std::cout << "Maze Game" << std::endl;
 
+void displayDescription(){
+  std::string description =
+  "==================== Maze Game ====================\n"
+  "I: entrance,    O: exit,       P: current position,\n"
+  "w: wall         t: small trap, T: large trap,\n" 
+  "H: hidden trap, x: fog,      ' ': path\n" 
+  "===================================================\n";
+  std::cout << description << std::endl;
+}
+
+
+int main(int argc, char const *argv[]) {
   if (argc < 2) {
     std::cout << "No file." << std::endl;
     return 1;
   }
+  
+  displayDescription();
 
   // Get maze string.
   std::string maze_str = parseFile(argv[1]);
